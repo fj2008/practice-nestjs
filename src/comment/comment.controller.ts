8bc4c7c55ac7177bc07aa532879db89482e63bd8
fj2008 <fj2008@naver.com>
2022-01-28 15:34:22 +0900

    comment test

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('comment')
-export class CommentController {}
+export class CommentController {
+  @Get()
+  testComment() {
+    return '안녕하다';
+  }
+}
